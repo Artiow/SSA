bea6dccc5c959cdb1495d3bf91f1de82ec38c0a3
Artiow <namednev_a@mail.ru>
2018-03-14 16:32:32 +0300

    lab2 mod

--- Lab2/logic/mbabuilder.h
+++ Lab2/logic/mbabuilder.h
@@ -8,8 +8,6 @@
 #include <iostream>
 #include <vector>
 
-#include "../../Lab1/logic/glue.h"
-
 using namespace std;
 
 vector<int> buildMaxBorderArray(const string &s) {
@@ -17,16 +15,11 @@ vector<int> buildMaxBorderArray(const string &s) {
     vector<int> array(n);
 
     for (int i = 0; i < (n - 1);) {
-        int tmp = (array[i] - 1);
-        while ((tmp > -1) && (array[i + 1] != array[tmp + 1])) tmp = (array[tmp] - 1);
-        if (array[i + 1] == array[tmp + 1]) {
-            i++;
-            tmp += 2;
-            array[i] = tmp;
-        } else {
-            i++;
-            array[i] = 0;
-        }
+        int tmp = array[i];
+        while ((tmp > 0) && (array[i + 1] != array[tmp])) tmp = (array[tmp + 1] - 1);
+
+        i++;
+        if (array[i] == array[tmp]) array[i] = ++tmp;
     }
 
     return array;
