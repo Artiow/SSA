3336e0189e0a8090c926f1e287a1e503aabff670
Artiow <namednev_a@mail.ru>
2018-03-20 01:55:21 +0300

    kmp online add

--- Lab2/logic/kmpsearch.h
+++ Lab2/logic/kmpsearch.h
@@ -34,4 +34,25 @@ vector<int> kmpSearch(const string &sample, const string &pattern, bool modified
 
 vector<int> kmpSearch(const string &sample, const string &pattern) { return kmpSearch(sample, pattern, false); }
 
+vector<int> kmpOnlineSearch(const string &sample, const string &pattern) {
+    unsigned int n = sample.length();
+    unsigned int m = pattern.length();
+
+    vector<int>* matrix = buildMaxBorderMatrix(pattern);
+    vector<int> occurrence;
+
+    int q = 0;
+    for (int i = 0; i < n; i++) {
+        if ((q > 0) && (pattern[q] != sample[i])) q = q - (matrix[sample[i]])[q - 1];
+        if (pattern[q] == sample[i]) q++;
+        if (q == m) {
+            q = (matrix[sample[i]])[m - 1];
+            occurrence.push_back(i - m + 1);
+        }
+    }
+
+    delete matrix;
+    return occurrence;
+}
+
 #endif //SSA_KMPSEARCH_H
