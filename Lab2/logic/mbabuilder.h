ecd9b43b3a0fcff04b59a5acf74f3822d4a7d6fb
Artiow <namednev_a@mail.ru>
2018-03-25 15:30:34 +0300

    kmp-online cut out

--- Lab2/logic/mbabuilder.h
+++ Lab2/logic/mbabuilder.h
@@ -51,32 +51,15 @@ int** buildMaxBorderMatrix(const string &s) {
     unsigned int n = s.length();
     unsigned int cardinality = getCardinality();
 
+    vector<int> array = buildMaxBorderArray(s, true);
     auto **matrix = new int *[cardinality];
     for (int i = 0; i < cardinality; i++) {
         matrix[i] = new int[n];
         for (int j = 0; j < n; j++) matrix[i][j] = 0;
     }
 
-    auto *array = new int(n);
-    array[0] = 0;
-
-    for (int i = 0; i < (n - 1);) {
-        int tmp = array[i];
-
-        i++;
-        char iResultChar = s[i];
-        while ((tmp > 0) && (iResultChar != s[tmp])) tmp = array[tmp - 1];
-        if (iResultChar == s[tmp]) array[i] = ++tmp; else array[i] = 0;
-    }
-
     int borderValue;
     char borderChar;
-
-    for (int i = 1; i < (n - 1); i++) {
-        borderValue = array[i];
-        if ((borderValue != 0) && (s[borderValue] == s[i + 1])) array[i] = array[borderValue - 1];
-    }
-
     for(int i = 0; i < n; i++){
         borderValue = array[i];
         borderChar = s[borderValue];
@@ -84,23 +67,6 @@ int** buildMaxBorderMatrix(const string &s) {
         (matrix[getArrayCode(borderChar)])[i] = borderValue;
     }
 
-//    =================================================================================================================
-//    FAST VERSION CUT OUT:
-//
-//    for (int i = 1; i < (n - 1); i++) {
-//        borderValue = array[i];
-//        borderChar = s[borderValue];
-//
-//        int newValue;
-//        if ((borderValue != 0) && (s[borderValue] == s[i + 1])) {
-//            newValue = array[borderValue - 1];
-//
-//            array[i] = newValue;
-//            (matrix[getArrayCode(borderChar)])[i] = newValue;
-//        } else (matrix[getArrayCode(borderChar)])[i] = array[i];
-//    }
-
-    delete array;
     return matrix;
 }
 
