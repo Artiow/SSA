03989c4373956e98de19603bce8b806da6848825
Artiow <namednev_a@mail.ru>
2018-03-25 15:35:51 +0300

    kmp-online add

--- Lab2/test/test.h
+++ Lab2/test/test.h
@@ -50,25 +50,22 @@ void test(const string &sample, const string &pattern, bool positions) {
 
     cout << "----------------------------------------------\n";
 
-//    =================================================================================================================
-//    KMP-ONLINE SEARCH CUT OUT:
-//
-//    cout << "\nKMP-ONLINE SEARCH\n";
-//    cout << "----------------------------------------------\n";
-//
-//    start_time = clock();
-//    result = kmpOnlineSearch(sample, pattern);
-//    end_time = clock();
-//
-//    cout << "Operating time: \t" << end_time - start_time << '\n';
-//    cout << "Num of occurrence: \t" << result.size() << '\n';
-//    if (positions && !result.empty()) {
-//        cout << "Positions: \t";
-//        for (auto item: result) cout << item << ' ';
-//        cout << '\n';
-//    }
-//
-//    cout << "----------------------------------------------\n";
+    cout << "\nKMP-ONLINE SEARCH\n";
+    cout << "----------------------------------------------\n";
+
+    start_time = clock();
+    result = kmpOnlineSearch(sample, pattern);
+    end_time = clock();
+
+    cout << "Operating time: \t" << end_time - start_time << '\n';
+    cout << "Num of occurrence: \t" << result.size() << '\n';
+    if (positions && !result.empty()) {
+        cout << "Positions: \t";
+        for (auto item: result) cout << item << ' ';
+        cout << '\n';
+    }
+
+    cout << "----------------------------------------------\n";
 }
 
 void test(const string &sample, const string &pattern) { test(sample, pattern, true); }
